4c27b7b45010078e83eac5567ce1e1b29cb85d0d
Mona <mghassemi@rally.io>
2022-04-10 12:14:37 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCnWzfcdhV8qvkhKHy7LDJfYAY28D9PBclTRMNcGn4GO5cWVA8unXlIGFX6iNK4i8iRuQo08KvlLMkd+5hUEOgpay3Sfw3dxc0kfhzL0ws7ESEnQ2rzd8XoTaNncD0If3uUFRlg9CxB/yP/eEpiJDyc4wzeBSTcnmDJFKjk/KsXa5CbUArSSp9H1MQUC1h0tDv6oqTyKjGerbadFg6QPBt6nIVtKESl7v5BA2ewGLXwBy0pNQJwwdA15cfpV8/7CJ0S9aw87l45futsFBwiDfZcnRKAadGF9LlnXwmjGbUISlADhQnurWx5xuI4PZSu5yEQ3cVhospTjJIljV0ZRJ2v',
 	}  
 }
