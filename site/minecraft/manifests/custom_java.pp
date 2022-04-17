7e9d53a829bfd771d3f40649dcd9722ffb774b2c
Mona <mghassemi@rally.io>
2022-04-17 13:34:42 -0400

    Blue mona patch 1 (#3)
    * Update Puppetfile
    
    Add java module to install proper java version
    
    * Update init.pp
    
    use puppetlabs-java to install correct java version
    
    * Update minecraft.service
    
    * Update Puppetfile
    
    add dependencies
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Create java.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update java.pp
    
    * Delete java.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update minecraft.service
    
    Use custom directory for java
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update minecraft.service
    
    * Update init.pp
    
    * Create custom_java.pp
    
    * Update init.pp
    
    * Update init.pp
    
    * Update custom_java.pp
    
    * Update init.pp

---
+++ site/minecraft/manifests/custom_java.pp
@@ -0,0 +1,14 @@
+class minecraft::custom_java { 
+  include java
+    file {'/custom': 
+      ensure => directory, 
+    }
+    java::adoptium { 'jdk17' :
+        ensure  => 'present',
+        version_major => '17',
+        version_minor => '0',
+        version_patch => '2',
+        version_build => '8',
+        basedir => '/custom/java',
+    }
+}
