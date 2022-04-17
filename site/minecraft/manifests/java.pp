5ede9964ebe0b128b1c33a371520a01fb6cb76b7
Mona <mghassemi@rally.io>
2022-04-17 09:38:39 -0400

    Update java.pp

--- site/minecraft/manifests/java.pp
+++ site/minecraft/manifests/java.pp
@@ -1,9 +1,7 @@
-  class { 'java' :
-    java::adoptium { 'jdk17' :
+java::adoptium { 'jdk17' :
       ensure  => 'present',
       version_major => '17',
       version_minor => '0',
       version_patch => '2',
       version_build => '1',
-    }
-  }
+}
